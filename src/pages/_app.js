7d2d7fb86cbbac4771ae6afe33ec8510dbc1fd49
ahmedabirdev <ahmedabir2852@gmail.com>
2025-12-16 21:56:39 +0530

    next script bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
 
   return (
-    <Component {...pageProps} />
+    <>
+      <Script
+        src="https://www.desmos.com/api/v1.9/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"
+        strategy="beforeInteractive"
+      />
+      <Component {...pageProps} />
+    </>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-scroll-behavior="smooth">
-      <Head>
-        <script
-          src="https://www.desmos.com/api/v1.9/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"
-          strategy="beforeInteractive"
-        />
-      </Head>
-
+      <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
